3721afc830de82982ea45d4f40bf7b51ad51fc52
Anna <annsta@gmail.com>
2016-10-20 19:26:23 +0300

    del

--- 01-Lab-DOM-and-Events/01-sum-numbers.js
+++
@@ -1,5 +0,0 @@
-function sumNumbers() {
-    let num1 = document.getElementById('num1').value;
-    let num2 = document.getElementById('num2').value;
-    document.getElementById('sum').value = Number(num1) + Number(num2);
-}
--- 01-Lab-DOM-and-Events/04-colorize-table.js
+++
@@ -1,7 +0,0 @@
-function colorizeTableRows() {
-    let rows = document.querySelectorAll('table tr');
-    for (let rowIndex in rows) {
-        if (rowIndex % 2 != 0)
-            rows[rowIndex].style.background = 'Teal';
-    }
-}
