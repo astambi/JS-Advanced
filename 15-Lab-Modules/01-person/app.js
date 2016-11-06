4197204fa1fddd4ba95cd000259cb2792c5e0cb3
Anna <annsta@gmail.com>
2016-11-06 21:36:07 +0200

    Lab: Modules

---
+++ 15-Lab-Modules/01-person/app.js
@@ -0,0 +1,10 @@
+// Obj 'result' required for testing in Judge
+
+let Person = require('./person').Person;    // with module.exports = {Person}
+result.Person = Person;
+
+// let Person = require('./person');        // with module.exports = {Person}
+// result.Person = Person.Person;
+
+// let Person = require('./person');        // with module.exports = Person
+// result.Person = Person;
