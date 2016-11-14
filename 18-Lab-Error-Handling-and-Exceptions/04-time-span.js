b0f21158d908b32ac68f0da445a7f50a926cb8f6
Anna <annsta@gmail.com>
2016-11-14 20:23:45 +0200

    Lab: Error Handling & Exceptions

---
+++ 18-Lab-Error-Handling-and-Exceptions/04-time-span.js
@@ -0,0 +1,27 @@
+class TimeSpan {
+    constructor(hours, minutes, seconds) {
+        if (!Number.isInteger(hours))
+            throw new RangeError("Invalid hours: " + hours);
+        if (!Number.isInteger(minutes))
+            throw new RangeError("Invalid minutes: " + minutes);
+        if (!Number.isInteger(seconds))
+            throw new RangeError("Invalid seconds: " + seconds);
+        this._seconds =
+            seconds + 60 * minutes + 60 * 60 * hours;
+    }
+
+    toString() {
+        let seconds = this._seconds % 60;
+        let minutes = Math.trunc(this._seconds / 60) % 60;
+        let hours = Math.trunc(this._seconds / (60 * 60));
+        seconds = ("0" + seconds).slice(-2);
+        minutes = ("0" + minutes).slice(-2);
+        return `${hours}:${minutes}:${seconds}`;
+    }
+}
+
+console.log('' + new TimeSpan(7, 8, 5));
+console.log('' + new TimeSpan(12, 76, -5));
+console.log('' + new TimeSpan('', 2.5,{})); // RangeError
+console.log('' + new TimeSpan(3, 2.5, {})); // RangeError
+console.log('' + new TimeSpan(3, 2, {}));   // RangeError
